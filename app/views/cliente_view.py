254a0a3d0545ba4247d2db40854076343cd30d22
Gabriel Dornelas <T780064@f1rst.com.br>
2022-03-31 14:56:07 -0300

    Initial commit

---
+++ app/views/cliente_view.py
@@ -0,0 +1,12 @@
+from app import app
+
+
+# @app.route("/ola", defaults={'nome': None})
+# @app.route("/ola/<string:nome>")
+# def teste(nome):
+#     return render_template("clientes/teste.html", nome_usuario=nome)
+#
+# @app.route("/oi")
+# def oi():
+#     return "Oi, mundo em Flask!!"
+
